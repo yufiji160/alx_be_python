24425a9eae23e58acadd994f695853877b2c20ae
yufiji160 <isaackkiobo0@gmail.com>
2025-07-28 16:02:56 +0300

    Arithmetic Operations Functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,4 @@
-# arithmetic_operations.py
-
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -9,7 +7,7 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            return "Error: Division by zero"
+            return "Error: Cannot divide by zero."
         return num1 / num2
     else:
-        return "Error: Invalid operation"
+        return "Error: Invalid operation."
