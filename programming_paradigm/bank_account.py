92e154765bc2eb78d1ea0e4211144ebc94c6ad8a
yufiji160 <isaackkiobo0@gmail.com>
2025-07-30 11:59:00 +0300

    Add bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+# bank_account.py
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance  # Encapsulated attribute
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
